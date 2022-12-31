0b31a50ba6c237c7f5093651569d73fa8403c4d5
CHENXIAOXIN\83745 <873456296@qq.com>
2022-12-31 23:42:10 +0800

    筛选

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 1322,
+    port: 1323,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
--- src/views/orderAll.vue
+++ src/views/orderAll.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="hello">
-    <mt-header title="订单中心">
+    <mt-header :title="titleName">
       <div slot="left">
         <mt-button  icon="back" @click="$router.go(-1)"></mt-button>
       </div>
     </mt-header>
+    <div class="fenlei_top">
+      <div    class="fenlei_top_left">
+        <input type="text" v-model.trim="queryParam.keyword" placeholder="搜索关键字（货号、尺码）" class="ins">
+      </div>
+      <div class="fenlei_top_right" >
+        <mt-button
+          style="margin-left: -60px"
+          type="primary"
+          size="small"
+          @click="search">搜索</mt-button>
+        <mt-button
+          style="margin-left: 14px;
+           width: 70px;"
+          type="primary"
+          size="small"
+          @click="isShowDialog2 = true">  <img style="margin-left: -10px;" src="../../static/img/choose.png" height="20" width="20" slot="icon">
+          <span style="margin-left: -5px;">筛选</span>
+          </mt-button>
+      </div>
+    </div>
     <mt-loadmore
+      style="padding-top: 0.86rem"
       :top-method="loadTop"
       :bottom-method="loadBottom"
       :bottom-all-loaded="allLoaded"
@@ -17,10 +38,12 @@
       <div class="dingdans_item" v-for="(item,index) in tableData" :key="index">
         <div class="dingdans_top">
           <div class="dingdans_top_left">
-            订单编号：<strong>{{item.orderNo}}</strong>
+            <strong>{{item.orderNo}}</strong>
           </div>
           <div class="dingdans_top_right">
-            状态：<strong>{{ item.status | dictToDescTypeValue(37) }} </strong>
+            <strong v-if="item.status == 7" class="color-success" >{{ item.status | dictToDescTypeValue(37) }} </strong>
+            <strong v-else-if="[3,4,5,6,8].includes(item.status)" class="color-danger" >{{ item.status | dictToDescTypeValue(37) }} </strong>
+            <strong v-else>{{ item.status | dictToDescTypeValue(37) }} </strong>
           </div>
         </div>
         <div class="dingdans_con">
@@ -73,10 +96,10 @@
         </div>
       </mt-header>
       <section style="height: 135vw;width: 80vw">
-        <mt-field label="货号" v-model="orderData.actNo" :readonly="true"></mt-field>
+        <mt-field label="货号" style="margin-top: 11vw;" v-model="orderData.actNo" :readonly="true"></mt-field>
         <mt-field label="尺码" v-model="orderData.size" :readonly="true"></mt-field>
         <mt-field label="状态">
-            <select style=";" v-model="requestParam.status">
+            <select  class="select80" v-model="requestParam.status">
               <option :disabled="true" value="" selected>请选择</option>
               <option v-for="x in statusList" :value="x.fieldValue">{{x.fieldName}}</option>
             </select>
@@ -105,16 +128,44 @@
         </div>
       </mt-header>
       <section style="height: 85vw;width: 80vw">
-        <mt-field label="货号"  v-model="orderData1.actNo" :readonly="1==1"></mt-field>
+        <mt-field label="货号" style="margin-top: 11vw;" v-model="orderData1.actNo" :readonly="1==1"></mt-field>
         <mt-field label="尺码" v-model="orderData1.size" :readonly="1==1"></mt-field>
         <mt-field label="运费" placeholder="请输入运费"  type="number" v-model="requestParam1.freight"></mt-field>
         <mt-field label="运单号" placeholder="请输入运单号"  v-model="requestParam1.waybillNo"></mt-field>
         <mt-field label="地址">
-            <select  v-model="requestParam1.addressId">
-          <option :disabled="true" value="" selected>请选择</option>
+            <select class="select80" v-model="requestParam1.addressId">
+              <option :disabled="true" value="" selected>请选择</option>
               <option v-for="x in addressList" :value="x.fieldValue">{{x.fieldName}}</option>
             </select>
         </mt-field>
+      </section>
+    </mt-popup>
+    <mt-popup
+      position="bottom"
+      v-model="isShowDialog2">
+      <mt-header title="筛选" >
+        <div slot="right">
+          <mt-button size="normal"  @click="resetHandle" style="font-size: 16px">关闭</mt-button>
+        </div>
+        <div slot="left">
+          <mt-button size="normal" @click="search1" style="font-size: 16px">确定</mt-button>
+        </div>
+      </mt-header>
+      <section style="height: 150vw;width: 100vw">
+        <mt-field label="状态" style="margin-top: 11vw;">
+            <select class="select100" v-model="queryParam.status" @change="changeSystem">
+               <option :disabled="true" value="" selected>请选择状态</option>
+              <option v-for="x in statusList" :value="x.fieldValue">{{x.fieldName}}</option>
+            </select>
+        </mt-field>
+        <mt-field label="地址">
+            <select class="select100" v-model="queryParam.addressId">
+          <option :disabled="true" value="" selected>请选择地址</option>
+              <option v-for="x in addressList" :value="x.fieldValue">{{x.fieldName}}</option>
+            </select>
+        </mt-field>
+        <mt-field label="运单号" placeholder="请输入运单号"  v-model="queryParam.waybillNo"></mt-field>
+        <mt-field label="订单号" placeholder="请输入订单号"  v-model="queryParam.orderNo"></mt-field>
       </section>
     </mt-popup>
     <div class="popContainer" v-if="pictureZoomShow" @click="pictureZoomShow = false">
@@ -163,6 +214,7 @@
           profits: ''
         },
         // popupVisible: false,
+        titleName: '订单',
         emtityMsg: '人家是有底线的 -.-',
         orderData: '',
         isShowDialog: false,
@@ -278,6 +330,7 @@
               }
             } else {
               this.allLoaded = true;
+              this.emtityMsg = '人家是有底线的 -.-'
               this.$toast('没有更多了')
             }
           } else {
@@ -285,6 +338,67 @@
             return false
           }
         })
+      },
+      search() {
+        if (!this.queryParam.keyword ) {
+          this.$toast('请输入关键字')
+          return
+        }
+        this.queryParam.pageNum = 1
+        this.allLoaded = false;
+        this.getPage()
+      },
+      changeSystem() {
+        // alert(1)
+        let sysDictList = localStorage.getItem('sysDictList') ? JSON.parse(
+          localStorage.getItem('sysDictList')) : []
+        let res = sysDictList.filter(
+          item => item.typeValue == 37 && item.fieldValue == this.queryParam.status)
+        // alert(res.length ? res[0].fieldName : '--')
+        this.titleName = res.length ? res[0].fieldName : ''
+        this.titleName = this.titleName + '订单'
+
+      },
+      search1() {
+        this.queryParam.pageNum = 1
+        this.allLoaded = false;
+        this.isShowDialog2 = false
+        this.getPage()
+      },
+      resetHandle() {
+        this.queryParam = {
+          id: '',
+          keyword: '',
+          orderNo: '',
+          inventoryId: '',
+          status: '',
+          shelvesPriceFrom: '',
+          shelvesPriceTo: '',
+          freightFrom: '',
+          freightTo: '',
+          poundageFrom: '',
+          poundageTo: '',
+          theirPriceFrom: '',
+          theirPriceTo: '',
+          addressId: '',
+          waybillNo: '',
+          createTimeFrom: '',
+          createTimeTo: '',
+          updateTimeFrom: '',
+          updateTimeTo: '',
+          sellTimeFrom: '',
+          sellTimeTo: '',
+          successTimeFrom: '',
+          successTimeTo: '',
+          pageSize: 10,
+          pageNum: 1
+        }
+        this.titleName = '订单'
+        this.createTime = ''
+        this.updateTime = ''
+        this.sellTime = ''
+        this.successTime = ''
+        this.search1()
       },
       handleTopChange(p_status) {
         this.topStatus = p_status;
@@ -459,16 +573,13 @@
 
   .dingdans_con_right_down {
     margin-top: 1.4vw;
-    font-size: 3.5vw;
+    font-size: 13px;
     margin-bottom: 2vw;
   }
   .dingdans_con_right_down_1 {
     margin-left: 26vw;
     margin-bottom: -7vw;
     font-size: 3.5vw;
-  }
-  .color-danger {
-    color: #F56C6C;
   }
 /*
  -----分割线---
@@ -497,7 +608,7 @@
   .hello {
     background-color: #F8FCFF;
     padding-top: 12vw;
-    font-size: 3.5vw;
+    font-size: 13px;
     height: 100vh;
     overflow-y: auto;
   }
@@ -551,5 +662,64 @@
   .mint-loadmore-top,
   .mint-loadmore-bottom {
     font-size: 0.28rem;
+  }
+
+  .fenlei_top {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 0.88rem;
+    padding: 0.1rem 0.2rem;
+    width: 100vw;
+    background: #eeeeee;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 99;
+    margin-top:0.85rem;
+  }
+
+  .fenlei_top_right {
+    font-size: 0.32rem;
+    color: #353535;
+    width: 2rem;
+    text-align: center;
+  }
+  .ins {
+    writing-mode: horizontal-tb !important;
+    font-style: ;
+    font-variant-ligatures: ;
+    font-variant-caps: ;
+    font-variant-numeric: ;
+    font-variant-east-asian: ;
+    font-weight: ;
+    font-stretch: ;
+    font-size: ;
+    font-family: ;
+    text-rendering: auto;
+    color: fieldtext;
+    letter-spacing: normal;
+    word-spacing: normal;
+    line-height: normal;
+    text-transform: none;
+    text-indent: 0px;
+    text-shadow: none;
+    display: inline-block;
+    text-align: start;
+    appearance: auto;
+    -webkit-rtl-ordering: logical;
+    cursor: text;
+    background-color: field;
+    margin: 0em;
+    padding: 1px 2px;
+    border-width: 2px;
+    border-style: inset;
+    border-color: -internal-light-dark(rgb(118, 118, 118), rgb(133, 133, 133));
+    border-image: initial;
+    border: 0;
+    outline: none;
+    width: 5.7rem;
+    padding: 0.2rem;
+
   }
 </style>
