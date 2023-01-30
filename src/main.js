cbdf9a0d9435c025222e5d520f0f1789ad88b799
CHENXIAOXIN\83745 <873456296@qq.com>
2023-01-30 13:50:38 +0800

    按虚引入

--- src/main.js
+++ src/main.js
@@ -1,29 +1,33 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import store from '@/vuex/store.js'   //vuex
 import api from '@/http/api.js'       //http请求
 import '@/utils/filter'
-import less from 'less'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import Mint from 'mint-ui';    //移动端UI
 import 'mint-ui/lib/style.css'
 import { Popup } from 'mint-ui';
-import VCharts from 'v-charts-v2'
-import vcharts from 'v-charts'
-Vue.use(vcharts )
-// set ElementUI lang to EN
-// Vue.use(ElementUI, { locale })
-// 如果想要中文版 element-ui，按如下方式声明
+// 引入echarts
+// import { VeLine } from 'v-charts-v2/lib/index.esm'
+import VeLine from 'v-charts-v2/lib/line'
+import VePie  from 'v-charts/lib/pie'
+
+
+// 挂载到vue实例中
+// Vue.prototype.$echarts = echarts
+// import VCharts from 'v-charts-v2'
+// import vcharts from 'v-charts'
+// Vue.use(vcharts )
+// Vue.use(VCharts);
 Vue.use(ElementUI)
 Vue.use(Mint);
-Vue.use(VCharts);
 Vue.config.productionTip = false
 Vue.prototype.$api = api;
 Vue.component(Popup.name, Popup);
-
+Vue.component(VeLine.name, VeLine)
+Vue.component(VePie.name, VePie)
 // if (localStorage.getItem('org_token_auth')) {
 // 用钩子函数beforeEach()对路由进行判断
 
